559ea8e219d90280f94db4b2aaec8e2df3816435
neginzahedi <zahedi.negin@gmail.com>
2022-04-06 23:07:36 -0400

    labs folder created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { Lab1Component} from './lab1/lab1.component'
 import { FooterComponent } from './footer/footer.component';
-import { Lab2Component } from './lab2/lab2.component';
-import { Lab3Component } from './lab3/lab3.component';
-import { Lab4Component } from './lab4/lab4.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// Labs
+import { Lab1Component } from './labs/lab1/lab1.component';
+import { Lab2Component } from './labs/lab2/lab2.component';
+import { Lab3Component } from './labs/lab3/lab3.component';
+import { Lab4Component } from './labs/lab4/lab4.component';
+import { Lab5Component } from './labs/lab5/lab5.component';
+// Assignments
 import { Assignment1Component } from './assignment1/assignment1.component';
 
 @NgModule({
@@ -24,13 +27,11 @@ import { Assignment1Component } from './assignment1/assignment1.component';
     Lab2Component,
     Lab3Component,
     Lab4Component,
-    Assignment1Component
+    Assignment1Component,
+    Lab5Component,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
