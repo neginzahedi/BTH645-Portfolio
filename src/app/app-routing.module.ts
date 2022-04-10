e33f6bc62233c451a6ded6ec662851323d97f145
neginzahedi <zahedi.negin@gmail.com>
2022-04-10 16:20:29 -0400

    lab5 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 // Labs
-import { Lab1Component } from './labs/lab1/lab1.component';
-import { Lab2Component } from './labs/lab2/lab2.component';
-import { Lab3Component } from './labs/lab3/lab3.component';
-import { Lab4Component } from './labs/lab4/lab4.component';
-import { Lab5Component } from './labs/lab5/lab5.component';
+import { Lab1Component } from './lab1/lab1.component';
+import { Lab2Component } from './lab2/lab2.component';
+import { Lab3Component } from './lab3/lab3.component';
+import { Lab4Component } from './lab4/lab4.component';
+import { Lab5Component } from './lab5/lab5.component';
 // Assignments
 import { Assignment1Component } from './assignment1/assignment1.component';
 const routes: Routes = [
